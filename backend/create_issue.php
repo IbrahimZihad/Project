de60ccbe3f8c66c76df58f203674926d10a9a9ac
IbrahimZihad <ibrahimzihad5@gmail.com>
2025-06-28 15:24:37 +0600

    vhange

--- backend/create_issue.php
+++ backend/create_issue.php
@@ -1,7 +1,7 @@
 <?php
 $data = json_decode(file_get_contents("php://input"), true);
 
-require_once 'db.php';
+include 'db.php';
 
 $stmt = $conn->prepare("INSERT INTO tasks (project_id, sprint_id, title, description, status, created_by) VALUES (?, ?, ?, ?, 'To Do', ?)");
 $stmt->bind_param("iissi", $data["project_id"], $data["sprint_id"], $data["title"], $data["description"], $data["created_by"]);
